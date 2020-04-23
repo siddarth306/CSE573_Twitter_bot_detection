8a2658ce1e0ba79669573d4e6e4f01ce61bdd5a9
Vivian So <vivianso@asu.edu>
2020-04-23 05:01:14 -0700

    updated to use transpose

--- pairwise_corr.py
+++ pairwise_corr.py
@@ -1,13 +1,23 @@
 import K_means as km
 import pandas as pd
+import pickle
 
-sample_filename = "nmf-for-week-1.pkl"
+def read_nmf_data(filename):
+    dict = pickle.load(open(filename, 'rb'))
+    # convert dict to array
+    users = []
+    values = []
+    for key in dict:
+        users.append(key)
+        values.append(dict[key])
+    return users, values
 
-#get the data points in each cluster
-clusters_of_coordinates = km.get_cluster_stat(sample_filename)
+def compute_coor(data):
+    df = pd.DataFrame(data).transpose()
+    coor_matrix = df.corr(method='pearson')
+    coor_matrix[coor_matrix < .90] = 0 #filter
+    return coor_matrix
 
-# To find the correlation among the columns using pearson method
-# StackOverflow: When the correlation coefficient is calculated, you divide by the standard deviation, which leads to a NA.
-df = pd.DataFrame(clusters_of_coordinates[3])
-coor_matrix = df.corr(method ='pearson')
-print(coor_matrix)
+
+
+

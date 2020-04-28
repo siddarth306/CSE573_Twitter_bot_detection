639e6950393a6151e7cc9da1cad7c618c6ee6437
Siddharth Kumar <siddarth306@gmail.com>
2020-04-27 23:15:50 -0700

    Added improved NMF calculation and new approach code

--- calcNMF.py
+++ calcNMF.py
@@ -4,26 +4,24 @@
 #
 
 
-INPUT_DIR = "./DataSet/vectors/user-vectors-for-week-{}.pkl"
-NMF_DIR = "./DataSet/nmf/nmf-for-week-{}.pkl"
-NP_DIR = "./DataSet/numpy_matrices/np-for-week-{}.pkl"
-WEEKS_DIR = "./DataSet/weeks/data-from-week-{}.pkl"
 import pickle
 from sklearn.decomposition import NMF
 import multiprocessing
 import numpy as np
 import sys
+import os
 
-def calc_NMF_matrix(week):
-    print("Computing NMF for week {}".format(week))
-    week_f = open(WEEKS_DIR.format(week), "rb")
+
+def calc_np_matrix(week, tool_config):
+    print("Computing np matrix for week {}".format(week))
+    week_f = open(tool_config["weeks_dir"].format(week), "rb")
     
     week_data = pickle.load(week_f)
     max_vector_size = len(week_data["retweet_tid"].unique())
     
-    del week_f
-    f1 = open(INPUT_DIR.format(week), "rb")
+    f1 = open(tool_config["user_vector_dir"].format(week), "rb")
     data = pickle.load(f1)
+    f1.close()
     vector_matrix = []
     users = list(data.keys())
     for name in users:
@@ -39,28 +37,77 @@ def calc_NMF_matrix(week):
     print("Created vector matrix")
     np_mat = np.array(vector_matrix)
     del vector_matrix
-    print("Running NMF")
-    with open(NP_DIR.format(week), "wb") as np_f1:
+    print("Saving np matrix")
+    with open(tool_config["np_dir"].format(week), "wb") as np_f1:
         pickle.dump(np_mat, np_f1)
-    #model = NMF(n_components=50, init='random', random_state=0)
-    #W = model.fit_transform(np_mat)
+    return []
 
-    #NMF_dict = {}
 
-    #for idx, name in enumerate(users):
-    #    NMF_dict[name] = W[idx]
+def calc_NMF_matrix(week, tool_config):
+    try:
+        f = open(tool_config["nmf_dir"].format(week), "r")
+        f.close()
+        print("file " + tool_config["nmf_dir"].format(week) + " exists.. ignoring")
+        return []
+    except FileNotFoundError:
+        pass
 
-    #nmf_f = open(NMF_DIR.format(week), "wb")
-    #pickle.dump(NMF_dict, nmf_f)
-    #print("Finished {}".format(week))
-    #return []
-
+    with open(tool_config["np_dir"].format(week), "rb") as np_f1:
+        np_mat = pickle.load(np_f1)
+    if len(np_mat) == 0:
+        return []
+    f1 = open(tool_config["user_vector_dir"].format(week), "rb")
+    data = pickle.load(f1)
+    f1.close()
+    users = list(data.keys())
  
-week = sys.argv[1]
-#weeks_range = range(11,37)
-#for week in weeks_range:
-calc_NMF_matrix(week)
-#pool = multiprocessing.Pool(processes=3)
-#results = [pool.apply_async(calc_NMF_matrix, args=(week,)) for week in range(11,37)]
-#output = [p.get() for p in results]
-import ipdb;ipdb.set_trace()
+    print("Computing NMF for week {}".format(week))
+    
+    model = NMF(n_components=50, init='random', random_state=0)
+    W = model.fit_transform(np_mat)
+
+    NMF_dict = {}
+
+    for idx, name in enumerate(users):
+        NMF_dict[name] = W[idx]
+
+    nmf_f = open(tool_config["nmf_dir"].format(week), "wb")
+    pickle.dump(NMF_dict, nmf_f)
+    print("Finished {}".format(week))
+    return []
+
+def main():
+    if len(sys.argv) != 2:
+        timeperiod = 1
+    else:
+        timeperiod = int(sys.argv[1])
+
+    BASE_DIR = "./DataSet_{}_day/".format(timeperiod)
+    tool_config = {
+        "base_dir": BASE_DIR,
+        "user_vector_dir": BASE_DIR + "vectors/user-vectors-for-week-{}.pkl",
+        "nmf_dir" : BASE_DIR + "nmf/nmf-for-week-{}.pkl",
+        "np_dir": BASE_DIR + "numpy_matrices/np-for-week-{}.pkl",
+        "weeks_dir": BASE_DIR + "weeks/data-from-week-{}.pkl",
+    }
+    count = len([name for name in os.listdir(tool_config["base_dir"] + "vectors") if ".pkl" in name])
+
+    #week = sys.argv[1]
+    #weeks_range = range(11,37)
+    #for week in weeks_range:
+    #    calc_NMF_matrix(week)
+    #pool = multiprocessing.Pool(processes=3)
+    #results = [pool.apply_async(calc_np_matrix, args=(week,tool_config)) for week in range(1,count+1)]
+    #output = [p.get() for p in results]
+    pool = multiprocessing.Pool(processes=3)
+    results = [pool.apply_async(calc_np_matrix, args=(week,tool_config)) for week in range(1,count+1)]
+    output = [p.get() for p in results]
+
+
+    pool = multiprocessing.Pool(processes=3)
+    results = [pool.apply_async(calc_NMF_matrix, args=(week,tool_config)) for week in range(1,count+1)]
+    output = [p.get() for p in results]
+
+
+if __name__ == "__main__":
+    main()
--- new_approach.py
+++ new_approach.py
@@ -2,89 +2,110 @@ import pickle
 import pandas as pd
 from BFS import Graph
 from pairwise_corr import read_nmf_data, compute_coor
-from pyvis.network import Network
+#from pyvis.network import Network
 from tqdm import tqdm
+#from collections import defaultdict
+import os
+import multiprocessing
 
-sample_filename = "/home/sid/Projects/CSE573_Twitter_bot_detection/DataSet_7_day/nmf/nmf-for-week-12.pkl"
+def calc_corr(week):
+    filename = "DataSet/nmf/nmf-for-week-{}.pkl".format(week)
+    
+    # NMF-> compute pairwise -> find clusters
+    users, values = read_nmf_data(filename)
+    data = compute_coor(values) #return correlation matrix
+    return data, users
 
-# NMF-> compute pairwise -> find clusters
-users, values = read_nmf_data(sample_filename)
-data = compute_coor(values) #return correlation matrix
-print(data)
-# dummy data to test BFS
-# users=["u1","u2","u3","u4"]
-# data=[[1,1,0,0],
-#       [1,1,0,0],
-#       [0,0,1,1],
-#       [0,0,1,1]]
+def save_corr(week,data, users):
+    data_dict = {}
+    for i in range(len(data)-1):
+        for j in range(i+1, len(data)):
+            if data_dict.get(users[i], None) is None:
+                data_dict[users[i]] = {}
+            if data_dict.get(users[j], None) is None:
+                data_dict[users[j]] = {}
+            data_dict[users[i]][users[j]] = data[i][j]
+            data_dict[users[j]][users[i]] = data[j][i]
 
-# find clusters
-# # nodes=3772, edges=1849808
-g = Graph()
-start = 1
-for i in tqdm(range(0, len(data))):
-    for j in range(start, len(data)):
-        if (data[i][j] >= 0.995):
-            g.addEdge(i, j)
-            g.addEdge(j, i)
-    start += 1
+    with open("DataSet/correlation/correlation-for-week-{}.pkl".format(week), "wb") as corr_f:
+        pickle.dump((data_dict), corr_f)
+    return None
 
-#find clusters
-clusters=[]
-root=0
-nodes = set(g.graph.keys()) # need this list so i can find the next nodes to run BFS on
-global_visited = set()
-for node in nodes:
-    if node not in global_visited:
-        nodes_visited = g.BFS(node, global_visited, data)
+def calc_corr_and_save(week):
+    data, users = calc_corr(week)
+    #save_corr(week, data, users)
+    return data, users
 
-        clusters.append(nodes_visited)
-        print(nodes_visited)
-        global_visited.update(nodes_visited) # remove nodes visited graph
+def calc_clusters(week): 
 
-    #nodes = nodes-set(nodes_visited)
-    #if len(nodes)==0: #ran out of nodes
-    #    break;
-    #root = list(nodes)[0] #get first item
+    try:
+        f1 = open("DataSet/correlation/correlation-for-week-{}.pkl".format(week), "rb")
+        f1.close()
+        print("Correlation for week {} exists... omitting".format(week))
+    except FileNotFoundError:
+        print("Calculating correlation for week {}".format(week))
+        data, users = calc_corr_and_save(week)
+        print("Calculating clusters for week {}".format(week))
+        # find clusters
+        # # nodes=3772, edges=1849808
+        g = Graph()
+        start = 1
+        for i in tqdm(range(0, len(data))):
+            for j in range(start, len(data)):
+                if (data[i][j] >= 0.995):
+                    g.addEdge(i, j)
+                    g.addEdge(j, i)
+            start += 1
+        
+        #find clusters
+        clusters=[]
+        root = 0
+        nodes = set(g.graph.keys()) # need this list so i can find the next nodes to run BFS on
+        global_visited = set()
+        for node in nodes:
+            if node not in global_visited:
+                nodes_visited = g.BFS(node, global_visited, data)
+        
+                clusters.append(nodes_visited)
+                print(nodes_visited)
+                global_visited.update(nodes_visited) # remove nodes visited graph
+        
+            #nodes = nodes-set(nodes_visited)
+            #if len(nodes)==0: #ran out of nodes
+            #    break;
+            #root = list(nodes)[0] #get first item
+        
+        named_clusters = []
+        for cluster in clusters:
+            named_single_cluster = []
+            for member in cluster:
+                named_single_cluster.append(users[member])
+            named_clusters.append(named_single_cluster)
+        
+        botnames = pd.read_csv("botnames.csv")
+        week_bots = set(botnames["BotName"].tolist()).intersection(set(users))
+        calculated_bots = set()
+        for idx, cluster in enumerate(named_clusters): 
+            cluster_set = set(cluster) 
+            cluster_bots = cluster_set.intersection(week_bots) 
+            calculated_bots.update(cluster_bots) 
 
-named_clusters = []
-for cluster in clusters:
-    named_single_cluster = []
-    for member in cluster:
-        named_single_cluster.append(users[member])
-    named_clusters.append(named_single_cluster)
-
-botnames = pd.read_csv("/home/sid/Projects/CSE573_Twitter_bot_detection/botnames.csv")
-week_bots = set(botnames["BotName"].tolist()).intersection(set(users))
-calculated_bots = set()
-for idx, cluster in enumerate(named_clusters): 
-    cluster_set = set(cluster) 
-    cluster_bots = cluster_set.intersection(week_bots) 
-    if len(cluster_bots) > 0: 
-        print(cluster_bots) 
-    calculated_bots.update(cluster_bots) 
-    total = 0
-    count = 0
-    non_members_total = 0
-    for u_idx, u in enumerate(clusters[idx][:-1]):
-        for v in clusters[idx][u_idx+1:]:
-            total += data[u][v]
-            if data[u][v] < 0.995:
-                non_members_total += 1
-            count += 1
-
-    print(len(cluster), len(cluster_bots), total/count, non_members_total, clusters[idx], cluster)
-
-users_dict = {}
-for idx, u in enumerate(users):
-    users_dict[u] = idx
-week_bots_ids = []
-for bot in week_bots: 
-    week_bots_ids.append(users_dict[bot]) 
-      
-
-import ipdb;ipdb.set_trace()
-print(clusters)
+        with open("DataSet/new_approach/new_approach-for-week-{}.pkl".format(week), "wb") as cluster_f:
+            pickle.dump((named_clusters,calculated_bots, len(calculated_bots)/len(week_bots)), cluster_f)
+        print("Done for week {}".format(week))
+        return [] 
 
 
-
+def main():
+    number_of_files = len(os.listdir('DataSet/nmf'))
+    pool = multiprocessing.Pool(processes=20)
+    #results = [pool.apply_async(calc_node2vec, args=(week,)) for week in range(1,number_of_files+1)]
+    results = [pool.apply_async(calc_clusters, args=(week,)) for week in range(6, 12)]
+    for p in results:
+        p.get()
+    pool.close()
+    pool.terminate()
+    pool.join()
+    
+if __name__ == '__main__':
+    main()
